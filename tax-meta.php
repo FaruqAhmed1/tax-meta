de69bfceb4702cb325e6c156ab4cfd4c927994bb
Faruq Ahmed <faruqahmedroni@gmail.com>
2023-08-12 02:22:07 +0600

    post meta

--- tax-meta.php
+++ tax-meta.php
@@ -14,19 +14,39 @@
  */
 
 class TaxMeta {
+
  function __construct(){
+
     add_action('plugin_loaded',array( $this,'taxm_plugin_text_domain' ));
+
     add_action( 'init',array($this,'taxm_bootstrap') );
+
     add_action('category_add_form_fields', array( $this,'taxm_category_form_fields' ) );
+
     add_action('post_tag_add_form_fields', array( $this,'taxm_category_form_fields' ) );
+
     add_action( 'create_category',array( $this,'taxm_save_category_field' ) );
+
     add_action( 'create_post_tag',array( $this,'taxm_save_category_field' ) );
+
     add_action('category_edit_form_fields', array( $this,'taxm_category_edit_form_fields' ) );
+
     add_action('post_tag_edit_form_fields', array( $this,'taxm_category_edit_form_fields' ) );
+
     add_action( 'edit_category',array( $this,'taxm_update_category_field' ) );
+
     add_action( 'edit_post_tag',array( $this,'taxm_update_category_field' ) );
+
+    add_action('admin_menu',array( $this,'taxm_add_metabox' ));
+
+    add_action( 'admin_enqueue_scripts', array( $this, 'taxm_admin_assets' ) );
  }
 
+
+ function taxm_admin_assets() {
+      wp_enqueue_style( 'taxm-admin-style', plugin_dir_url( __FILE__ ) . "assets/admin/css/style.css", null, time() );
+
+  }
   function taxm_update_category_field($term_id){
       if(wp_verify_nonce($_POST['_wpnonce'],"update-tag_{$term_id}")){  
           $exta_info = sanitize_text_field($_POST['extra-info']);   
@@ -75,6 +95,53 @@ function taxm_bootstrap(){
       );
       register_meta( 'term','taxm_extra_info',$args );
 }
+
+
+//  Post meta 
+
+ function taxm_add_metabox(){
+      add_meta_box(
+            'omb_select_post',
+            __( 'Select Post', 'tax-meta' ),
+            array( $this, 'taxm_display_post' ),
+            array( 'page' )
+        );
+ }
+
+ function taxm_display_post(){
+      
+        wp_nonce_field( 'taxm_posts', 'omb_posts_nonce' );
+        $label = esc_html__( 'Select Post','tax-meta' );
+
+        $metabox_html = <<<EOD
+        <div class="fields">
+            <div class="field_c">
+                <div class="label_c">
+                    <label>{$label}</label>
+                </div>
+                <div class="input_c">
+                    <select name="taxm_select_post" id="taxm_select_post">
+                    <option value="0">{$label}</option>
+                    </select>
+                </div>
+                <div class="float_c"></div>
+            </div>
+            
+        </div>
+        EOD;
+
+        echo $metabox_html;
+ }
+
+
+
+
+
+
+
+
+
+
 
  function taxm_plugin_text_domain(){
     load_plugin_textdomain('tax-meta',false,dirname(__FILE__).'/languages');
